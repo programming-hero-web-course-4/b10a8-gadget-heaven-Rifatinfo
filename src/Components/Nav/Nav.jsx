66b06918ccb2a755b81263cf232c02a07f68bc0f
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-01-20 13:12:00 +0600

    loadinger spinner add

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from "react-router-dom"; // Correct import
 const Nav = ({ route }) => {
     return (
         <div className="mt-4 md:mt-0">
@@ -6,9 +7,16 @@ const Nav = ({ route }) => {
                     className="hover:bg-white hover:text-black hover:rounded-lg px-28 md:px-6 py-3 md:py-2 transition-all 
                     duration-200 ease-in-out md:bg-transparent text-center"
                 >
-                    <a href={route.path} className="">
+                    <NavLink
+                        to={route.path} // Use "to" instead of "href"
+                        className={({ isActive }) =>
+                            isActive
+                                ? "text-black underline" // Classes for active state
+                                : "" // Classes for inactive state
+                        }
+                    >
                         {route.name}
-                    </a>
+                    </NavLink>
                 </li>
             </ul>
         </div>
--- src/Roots/Roots.jsx
+++ src/Roots/Roots.jsx
@@ -1,13 +1,33 @@
-import { Outlet } from "react-router";
+import { Outlet, useNavigation } from "react-router";
 import Navbar from "../Components/Nav/Navbar";
 import Footer from "../Components/Footer/Footer";
+import { ThreeDots } from "react-loader-spinner";
 
 const Roots = () => {
+    const navigation = useNavigation();
     return (
         <div>
-            <Navbar/>
-            <Outlet/>
-            <Footer/>
+            <Navbar />
+            {
+                navigation.state === "loading" ?
+                    <>
+                        <div className="flex justify-center items-center md:h-screen">
+                            <div>
+                                <ThreeDots
+                                    visible={true}
+                                    height="80"
+                                    width="80"
+                                    color="#4fa94d"
+                                    radius="9"
+                                    ariaLabel="three-dots-loading"
+                                    wrapperStyle={{}}
+                                    wrapperClass=""
+                                />
+                            </div>
+                        </div>
+                    </> : <Outlet />
+            }
+            <Footer />
         </div>
     );
 };
