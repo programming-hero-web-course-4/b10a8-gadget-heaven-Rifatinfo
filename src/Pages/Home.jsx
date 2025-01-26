58cbd3dd8ffc62001eb9841e476f76c2e1ebc1a4
Rifatinfo <mdrifathossainsinfo@gmail.com>
2025-01-27 00:18:52 +0600

    some error

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,28 +1,31 @@
 
+
 import { useState } from "react";
 import { Link, useLoaderData} from "react-router";
 
 const Home = () => {
     const gadgets = useLoaderData();
     // console.log(gadgets);
-    const [sort, setSort] = useState([]);
+    const [filteredItems, setFilteredItems] = useState(gadgets);
 
-    const handleSortingItem = (itemType) =>{
-        setSort(itemType);
-        if(itemType === "Laptops"){
-            const item = [...sort].sort((a, b) => a.name - b.name);
-            setSort(item);
+    const handleFilteringItem = (itemType) => {
+        if(itemType === "All Accessories"){
+            setFilteredItems()
         }
-    }
+        const filtered = gadgets.filter((item) => item.type === itemType); // Filter items by type
+        setFilteredItems(filtered);
+      };
+    // const sortedItemsType = [...filteredItem].sort((a, b) => a.type.localeCompare(b.type));
     return (
         <div className="mt-10 max-w-7xl mx-auto">
             <div className="grid grid-cols-1 gap-2 md:flex md:gap-4">
                 <div className="w-full p-4 md:w-1/5 border shadow-lg rounded-lg">
                    <div className="md:grid md:grid-cols-1 md:gap-4 md:ml-8 grid grid-cols-2 gap-2">
-                      <button onClick={() => handleSortingItem("Laptops")} className="btn btn-outline text-[#9538E2]">Laptops</button>
-                      <button onClick={() => handleSortingItem("Phones")} className="btn btn-outline text-[#9538E2]">Phones</button>
-                      <button onClick={() => handleSortingItem("Accessories")} className="btn btn-outline text-[#9538E2]">Accessories</button>
-                      <button onClick={() => handleSortingItem("Smart Watches")} className="btn btn-outline text-[#9538E2]">Smart Watches</button>
+                      <button onClick={() => handleFilteringItem("Laptops")} className="btn btn-outline text-[#9538E2]">All Accessories</button>
+                      <button onClick={() => handleFilteringItem("Laptops")} className="btn btn-outline text-[#9538E2]">Laptops</button>
+                      <button onClick={() => handleFilteringItem("Phones")} className="btn btn-outline text-[#9538E2]">Phones</button>
+                      <button onClick={() => handleFilteringItem("Accessories")} className="btn btn-outline text-[#9538E2]">Accessories</button>
+                      <button onClick={() => handleFilteringItem("Smart Watches")} className="btn btn-outline text-[#9538E2]">Smart Watches</button>
                    </div>
                 </div>
                 <div className="w-full md:w-4/5 border  shadow-lg rounded-lg">
